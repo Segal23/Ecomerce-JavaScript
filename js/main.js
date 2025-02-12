377aef966de1fbd9eee9c8310a5e7df488a97cff
Sebastián Gallegos <seba.gallegosu@gmail.com>
2025-02-12 14:26:36 -0300

    Se fuerza la recarga de la página al darle atrás

--- js/main.js
+++ js/main.js
@@ -6,16 +6,21 @@ const baseURL =  window.location.origin.includes("github.io")
 
 //Se fuerza la cargar la página al darle atrás en lugar de ir por el menú
 //ya que localmente funciona bien pero desde Github no se actualizan algunos valores
-window.addEventListener("pageshow", (event) => {
-    console.log("Evento pageshow detectado, persisted:", event.persisted);
+// window.addEventListener("pageshow", (event) => {
+//     console.log("Evento pageshow detectado, persisted:", event.persisted);
 
-    if (event.persisted) {
-        location.reload(); 
-    } else {
+//     if (event.persisted) {
+//         location.reload(); 
+//     } else {
+//         setSesionUsuario();
+//         setSessionValues();
+//     }
+// });
+
+window.onload = function () {
         setSesionUsuario();
         setSessionValues();
-    }
-});
+};
 
 class Producto{
 
